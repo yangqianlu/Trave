34038b3b192cc7f978cf450c76376b7c53127a31
杨前露 <348680298@qq.com>
2018-05-29 15:53:41 +0800

    添加注释

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import './assets/styles/reset.css'
-import  './assets/styles/border.css'
-import fastClick from 'fastclick' 
-import './assets/styles/iconfont.css'
+import './assets/styles/reset.css'// 重置样式
+import  './assets/styles/border.css' //手机边框显示2px
+import fastClick from 'fastclick'  //手机点击延迟300ms
+import './assets/styles/iconfont.css' //字体库
 fastClick.attach(document.body) //fastclick里面的方法
 Vue.config.productionTip = false
 
