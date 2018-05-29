9fe18718fb87dea192c8d98123b47dc4822973e5
杨前露 <348680298@qq.com>
2018-05-29 15:15:30 +0800

    first

---
+++ src/components/HomeHeader.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont leftArrow">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont searchIcon">&#xe632;</span>
+      <input type="text" placeholder="输入城市">
+    </div>
+    <div class="header-right">城市 <span class="iconfont">&#xe64a;</span></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="stylus">
+@import '~@/assets/styles/varibles.styl'
+  .header
+    line-height:0.86rem
+    background: $bgcolor
+    color #fff
+    width:100%
+    display :flex
+    .header-left
+      width 0.64rem
+      .leftArrow
+        text-align :center
+        font-size :0.4rem
+    .header-right
+      width :1.24rem
+      margin-left:0.1rem
+    .header-input
+      
+      flex:1
+      position:relative
+      .searchIcon
+       position:absolute
+       top:0
+       left:5px
+       color:#ccc
+      input 
+        padding-left:30px
+        box-sizing :border-box
+        width:100% 
+        height :0.64rem
+        border-radius:0.1rem
+</style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="Home">
+        <home-header></home-header>
+    </div>
+</template>
+<script>
+import HomeHeader from '@/components/HomeHeader'
+export default {
+    name:'Home',
+    components:{
+        HomeHeader
+    }
+}
+</script>
+<style>
+
+</style>
+
+
