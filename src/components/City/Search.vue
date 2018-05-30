1a194a9b016c8f8d1883a55693fd1902c34f9f0e
杨前露 <348680298@qq.com>
2018-05-30 14:45:36 +0800

    search

---
+++ src/components/City/Search.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="search">
+        <input type="text" placeholder="请输入城市名或拼音">
+    </div>
+</template>
+
+<script>
+export default {
+    name:'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+ @import  '~@/assets/styles/varibles.styl'
+ .search
+  
+    height :0.76rem
+    line-height :0.76rem
+    background :$bgcolor
+    padding:0 0.1rem
+    margin-top:-1px
+    input 
+        width :100%
+        height :0.64rem;
+        line-height :0.64rem
+        text-align center
+        color #333
+        padding:0 0.1rem
+        box-sizing :border-box
+        border-radius:5px
+
+
+
+</style>
--- src/pages/City.vue
+++ src/pages/City.vue
@@ -1,14 +1,17 @@
 <template>
     <div class="city">
         <header-city></header-city>
+        <city-search></city-search>
     </div>
 </template>
 <script>
 import HeaderCity from '@/components/City/HeaderCity'
+import CitySearch from '@/components/City/Search'
 export default {
     name:'City',
     components:{
-        HeaderCity
+        HeaderCity,
+        CitySearch
     }
 }
 </script>
